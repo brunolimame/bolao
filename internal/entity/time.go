7333a3e632e6fec0fa7bee9fd594756ad826b83d
Bruno Lima <brunolimame@gmail.com>
2023-03-22 11:45:09 -0300

    struct do time

--- internal/entity/time.go
+++ internal/entity/time.go
@@ -6,16 +6,22 @@ import (
 )
 
 type Time struct {
-	ID     entity.ID `json:"id"`
-	Nome   string    `json:"nome"`
-	Criado time.Time `json:"criado"`
+	ID       entity.ID `json:"id"`
+	Nome     string    `json:"nome"`
+	Escudo   string    `json:"escudo"`
+	Criado   time.Time `json:"criado"`
+	Alterado time.Time `json:"alterado"`
+	Status   bool      `json:"status"`
 }
 
-func NewTime(nome string) (*Time, error) {
+func NewTime(nome string, escudo string) (*Time, error) {
 	time := &Time{
-		ID:     entity.NewID(),
-		Nome:   nome,
-		Criado: time.Now(),
+		ID:       entity.NewID(),
+		Nome:     nome,
+		Escudo:   escudo,
+		Criado:   time.Now(),
+		Alterado: time.Time{},
+		Status:   true,
 	}
 
 	return time, nil
--- internal/entity/time_test.go
+++ internal/entity/time_test.go
@@ -7,11 +7,13 @@ import (
 )
 
 func TestNewTime(t *testing.T) {
-	time, err := NewTime("Brasil")
+	time, err := NewTime("Brasil", "escudo.jpg")
 	assert.Nil(t, err)
 	assert.NotNil(t, time)
 	assert.NotEmpty(t, time.ID)
 	assert.NotEmpty(t, time.Nome)
 	assert.NotEmpty(t, time.Criado)
 	assert.Equal(t, "Brasil", time.Nome)
+	assert.Equal(t, "escudo.jpg", time.Escudo)
+	assert.Equal(t, true, time.Status)
 }
