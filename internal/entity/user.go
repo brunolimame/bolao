7d32aa011462a55028ecb8a78f33f909252d250c
Bruno Lima <brunolimame@gmail.com>
2023-04-06 11:35:18 -0300

    Finalizando teste unitários de userEntity

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -2,33 +2,73 @@ package entity
 
 import (
 	"bolao/pkg/entity"
+	"crypto/rand"
+	"fmt"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+const c_DEFAULT_COST_PASSWORD = bcrypt.DefaultCost
 
 type UserEntity struct {
 	ID       entity.ID `json:"id"`
 	Nome     string    `json:"nome"`
 	Email    string    `json:"email"`
 	Password string    `json:"-"`
-	Criado   string    `json:"criado"`
-	Alterado string    `json:"alterado"`
+	Salt     string    `json:"-"`
+	Criado   time.Time `json:"criado"`
+	Alterado time.Time `json:"alterado"`
+	Status   bool      `json:"status"`
 }
 
 func NewUser(nome, email, password string) (*UserEntity, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	newSalt, err := genSalt()
 	if err != nil {
 		return nil, err
 	}
-	return &UserEntity{
+
+	newUser := &UserEntity{
 		ID:       entity.NewID(),
 		Nome:     nome,
 		Email:    email,
-		Password: string(hash),
-	}, nil
+		Password: password,
+		Salt:     newSalt,
+		Criado:   time.Now(),
+		Alterado: time.Time{},
+		Status:   true,
+	}
+
+	passwordCrypt, err := newUser.EncryptPassword(newUser.Password + newUser.Salt)
+
+	if err != nil {
+		return nil, err
+	}
+	newUser.Password = string(passwordCrypt)
+
+	return newUser, nil
 }
 
-func (u *UserEntity) ValidarSenha(password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+func genSalt() (string, error) {
+	salt := make([]byte, c_DEFAULT_COST_PASSWORD)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", salt), nil
+}
+
+func (u *UserEntity) EncryptPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), c_DEFAULT_COST_PASSWORD)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (u *UserEntity) ValidPassword(password string) bool {
+	testPassword := password + u.Salt
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(testPassword))
 	return err == nil
 }
--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestUser(t *testing.T) {
 
-	t.Run("Nova usuário", func(t *testing.T) {
+	t.Run("Novo usuário", func(t *testing.T) {
 		user, err := NewUser("Bruno", "brunolimame@gmail.com", "123456")
 		assert.Nil(t, err)
 		assert.NotNil(t, user)
@@ -16,15 +16,19 @@ func TestUser(t *testing.T) {
 		assert.NotEmpty(t, user.Nome)
 		assert.NotEmpty(t, user.Email)
 		assert.NotEmpty(t, user.Password)
+		assert.NotZero(t, len(user.Password))
+		assert.NotEmpty(t, user.Salt)
 		assert.Equal(t, "Bruno", user.Nome)
 		assert.Equal(t, "brunolimame@gmail.com", user.Email)
+		assert.NotEmpty(t, user.Criado)
+		assert.Empty(t, user.Alterado)
+		assert.Equal(t, true, user.Status)
 	})
 
 	t.Run("Validar Password", func(t *testing.T) {
 		user, err := NewUser("Bruno", "brunolimame@gmail.com", "123456")
 		assert.Nil(t, err)
-		assert.True(t, user.ValidarSenha("123456"))
-		assert.False(t, user.ValidarSenha("1234567"))
-		assert.NotEqual(t, "123456", user.Password)
+		assert.False(t, user.ValidPassword("1234567"))
+		assert.True(t, user.ValidPassword("123456"))
 	})
 }
