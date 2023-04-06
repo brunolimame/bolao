4fbc7752aef4865a414aa9f0aa5c6572bbdbe8dd
Bruno Lima <brunolimame@gmail.com>
2023-04-06 12:26:24 -0300

    Adicionado pontos ao palpite

--- internal/entity/palpite.go
+++ internal/entity/palpite.go
@@ -17,18 +17,20 @@ type PalpiteEntity struct {
 	JogoID   string    `json:"jogo_id"`
 	PlacarA  int       `json:"placar_a"`
 	PlacarB  int       `json:"placar_b"`
+	Pontos   int       `json:"pontos"`
 	Criado   time.Time `json:"criado"`
 	Alterado time.Time `json:"alterado"`
 	Status   bool      `json:"status"`
 }
 
-func NewPalpite(playerId string, jogoId string, placarA int, placarB int) (*PalpiteEntity, error) {
+func NewPalpite(playerId string, jogoId string) (*PalpiteEntity, error) {
 	palpite := &PalpiteEntity{
 		ID:       entity.NewID(),
 		PlayerID: playerId,
 		JogoID:   jogoId,
-		PlacarA:  placarA,
-		PlacarB:  placarB,
+		PlacarA:  0,
+		PlacarB:  0,
+		Pontos:   0,
 		Criado:   time.Now(),
 		Alterado: time.Time{},
 		Status:   true,
@@ -50,6 +52,15 @@ func (p *PalpiteEntity) Validate() error {
 		return errors.New(PalpiteEntityMsgErrorPlayerRequerido)
 	}
 	return nil
+}
+
+func (p *PalpiteEntity) SetPlacar(golsTimeA int, golsTimeB int) {
+	p.PlacarA = golsTimeA
+	p.PlacarB = golsTimeB
+}
+
+func (p *PalpiteEntity) SetPontos(pontos int) {
+	p.Pontos = pontos
 }
 
 func (p *PalpiteEntity) Enable() {
--- internal/entity/palpite_test.go
+++ internal/entity/palpite_test.go
@@ -9,7 +9,7 @@ import (
 func TestPalpite(t *testing.T) {
 
 	t.Run("Criando novo palpite", func(t *testing.T) {
-		palpite, err := NewPalpite("p1", "j1", 1, 0)
+		palpite, err := NewPalpite("p1", "j1")
 		assert.Nil(t, err)
 		assert.NotNil(t, palpite)
 		assert.NotEmpty(t, palpite.ID)
@@ -17,15 +17,16 @@ func TestPalpite(t *testing.T) {
 		assert.Equal(t, "p1", palpite.PlayerID)
 		assert.NotEmpty(t, palpite.JogoID)
 		assert.Equal(t, "j1", palpite.JogoID)
-		assert.Equal(t, 1, palpite.PlacarA)
+		assert.Equal(t, 0, palpite.PlacarA)
 		assert.Equal(t, 0, palpite.PlacarB)
+		assert.Equal(t, 0, palpite.Pontos)
 		assert.NotEmpty(t, palpite.Criado)
 		assert.Empty(t, palpite.Alterado)
 		assert.Equal(t, true, palpite.Status)
 	})
 
 	t.Run("Alterando status", func(t *testing.T) {
-		palpite, _ := NewPalpite("p1", "j1", 1, 0)
+		palpite, _ := NewPalpite("p1", "j1")
 		assert.NotNil(t, palpite)
 
 		palpite.Disable()
@@ -35,15 +36,32 @@ func TestPalpite(t *testing.T) {
 		assert.Equal(t, true, palpite.Status)
 	})
 
+	t.Run("Adicionando Pontos", func(t *testing.T) {
+		palpite, _ := NewPalpite("p1", "j1")
+		assert.NotNil(t, palpite)
+		assert.Equal(t, 0, palpite.Pontos)
+		palpite.SetPontos(10)
+		assert.Equal(t, 10, palpite.Pontos)
+	})
+	t.Run("Adicionando placar", func(t *testing.T) {
+		palpite, _ := NewPalpite("p1", "j1")
+		assert.NotNil(t, palpite)
+		assert.Equal(t, 0, palpite.PlacarA)
+		assert.Equal(t, 0, palpite.PlacarB)
+		palpite.SetPlacar(5, 2)
+		assert.Equal(t, 5, palpite.PlacarA)
+		assert.Equal(t, 2, palpite.PlacarB)
+	})
+
 	t.Run("Criando palpite sem o jogo", func(t *testing.T) {
-		palpite, err := NewPalpite("p1", "", 1, 0)
+		palpite, err := NewPalpite("p1", "")
 		assert.Nil(t, palpite)
 		assert.NotNil(t, err)
 		assert.EqualError(t, err, PalpiteEntityMsgErrorIdJogoRequerido)
 	})
 
 	t.Run("Criando palpite sem o player", func(t *testing.T) {
-		palpite, err := NewPalpite("", "j1", 1, 0)
+		palpite, err := NewPalpite("", "j1")
 		assert.Nil(t, palpite)
 		assert.NotNil(t, err)
 		assert.EqualError(t, err, PalpiteEntityMsgErrorPlayerRequerido)
